fbff3add5bff246658503d7b8deb17635b99140a
murad <muradgus226@gmail.com>
2024-03-05 02:46:06 +0700

    Initial commit

---
+++ app/Models/User.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Models;
+
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+use Laravel\Sanctum\HasApiTokens;
+
+class User extends Authenticatable
+{
+    use HasApiTokens, HasFactory, Notifiable;
+
+    protected $fillable = ['first_name', 'last_name', 'email', 'password'];
+
+    public function files() {
+        return $this->hasMany(File::class);
+    }
+
+    public function rights() {
+        return $this->hasMany(Right::class);
+    }
+
+    public function generateToken() {
+        $this->remember_token = Hash::make(Str::random());
+        $this->save();
+        return $this->remember_token;
+    }
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+        'password' => 'hashed',
+    ];
+}
